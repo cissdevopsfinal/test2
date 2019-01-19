07fe3164d2668473fe3175d37f18b43f3ed0c7de
cissdevopsfinal <46851375+cissdevopsfinal@users.noreply.github.com>
2019-01-20 00:37:39 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        \Illuminate\Support\Facades\URL::forceScheme('https');
 
     }
 
